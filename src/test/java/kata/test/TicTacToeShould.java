04c34368010a5631314c74526bf9569702ef10dd
oawood93 <oa.wood@outlook.com>
2022-02-21 20:27:37 +0000

    red

--- src/test/java/kata/test/TicTacToeShould.java
+++ src/test/java/kata/test/TicTacToeShould.java
@@ -2,41 +2,34 @@ package kata.test;
 
 import kata.TicTacToe;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TicTacToeShould {
 
-    @Test
-    void place_x_first_move_on_bottom_left(){
+    private static String emptyRow = "_|_|_";
+
+    @ParameterizedTest
+    @MethodSource("xFirstMoveCases")
+    void place_x_first_move_on_bottom_left(int x, int y, String expected){
         var ticTacToe = new TicTacToe();
-        String expected = "_|_|_\n" +
-                          "_|_|_\n" +
-                          "X|_|_";
-        String result = ticTacToe.play(0,0);
+        String result = ticTacToe.play(x,y);
 
         assertEquals(expected, result);
     }
 
-    @Test
-    void place_x_first_move_on_bottom_middle(){
-        var ticTacToe = new TicTacToe();
-        String expected = "_|_|_\n" +
-                "_|_|_\n" +
-                "_|X|_";
-        String result = ticTacToe.play(1,0);
 
-        assertEquals(expected, result);
-    }
-
-    @Test
-    void place_x_first_move_on_bottom_right(){
-        var ticTacToe = new TicTacToe();
-        String expected = "_|_|_\n" +
-                "_|_|_\n" +
-                "_|_|X";
-        String result = ticTacToe.play(2,0);
-
-        assertEquals(expected, result);
+    private static Stream<Arguments> xFirstMoveCases(){
+        return Stream.of(
+                Arguments.of(0, 0, emptyRow + "\n" + emptyRow + "\n" + "X|_|_"),
+                Arguments.of(1, 0, emptyRow + "\n" + emptyRow + "\n" + "_|X|_"),
+                Arguments.of(2, 0, emptyRow + "\n" + emptyRow + "\n" + "_|_|X"),
+                Arguments.of(0, 1, emptyRow + "\n" + "X|_|_\n" + emptyRow)
+                );
     }
 }
