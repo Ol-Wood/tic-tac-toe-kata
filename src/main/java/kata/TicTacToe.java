776b715381c99895fae6282ff22a00a7e04308ae
oawood93 <oa.wood@outlook.com>
2022-02-21 20:21:26 +0000

    rfactor code

--- src/main/java/kata/TicTacToe.java
+++ src/main/java/kata/TicTacToe.java
@@ -2,14 +2,26 @@ package kata;
 
 public class TicTacToe {
     public String play(int x, int y) {
+        var gameSb = new StringBuilder();
+        gameSb.append("_|_|_\n");
+        gameSb.append("_|_|_\n");
 
-        if (x == 1){
-            return "_|_|_\n" +
-                    "_|_|_\n" +
-                    "_|X|_";
+        gameSb.append(buildRow(x));
+
+        return gameSb.toString();
+    }
+
+    private String buildRow(int x) {
+        var row = new String[3];
+
+        for (int i = 0; i < 3; i++) {
+            if (x == i) {
+                row[i] = "X";
+                continue;
+            }
+            row[i] = "_";
         }
-        return "_|_|_\n" +
-                "_|_|_\n" +
-                "X|_|_";
+
+        return String.join("|", row);
     }
 }
