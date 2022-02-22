fe5d15b05d29b3b360f1c1cfee8f987ef4de1c77
oawood93 <oa.wood@outlook.com>
2022-02-22 20:50:24 +0000

    refactor

--- src/main/java/kata/TicTacToe.java
+++ src/main/java/kata/TicTacToe.java
@@ -1,15 +1,16 @@
 package kata;
 
 public class TicTacToe {
-    public String play(int x, int y) {
 
-        if (x == 2){
+    public String play(Point point) {
+
+        if (point.x() == 2){
             return "_|_|X\n" +
                     "_|_|_\n" +
                     "_|_|_";
         }
 
-        if (x == 1){
+        if (point.x() == 1){
             return "_|X|_\n" +
                     "_|_|_\n" +
                     "_|_|_";
--- src/test/java/kata/test/TicTacToeShould.java
+++ src/test/java/kata/test/TicTacToeShould.java
@@ -1,7 +1,7 @@
 package kata.test;
 
+import kata.Point;
 import kata.TicTacToe;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -15,18 +15,18 @@ public class TicTacToeShould {
     private static String emptyLine = "_|_|_";
     @ParameterizedTest
     @MethodSource("xFirstMoveTopLine")
-    void place_x_first_move_on_top_left(int x, int y, String expected){
+    void place_x_first_move_on_top_left(Point input, String expected){
         var ticTacToe = new TicTacToe();
-        String result = ticTacToe.play(x,y);
+        String result = ticTacToe.play(input);
 
         assertEquals(expected, result);
     }
 
     private static Stream<Arguments> xFirstMoveTopLine(){
         return Stream.of(
-                Arguments.of(0, 0, "X|_|_\n" + emptyLine + "\n" + emptyLine),
-                Arguments.of(1, 0, "_|X|_\n" + emptyLine + "\n" + emptyLine),
-                Arguments.of(2, 0, "_|_|X\n" + emptyLine + "\n" + emptyLine));
+                Arguments.of(new Point(0, 0), "X|_|_\n" + emptyLine + "\n" + emptyLine),
+                Arguments.of(new Point(1, 0), "_|X|_\n" + emptyLine + "\n" + emptyLine),
+                Arguments.of(new Point(2, 0), "_|_|X\n" + emptyLine + "\n" + emptyLine));
     }
 
 
