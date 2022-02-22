1368ebc32f490fc34e4305e831a42fa3c535ccd4
oawood93 <oa.wood@outlook.com>
2022-02-22 20:47:06 +0000

    green

--- src/main/java/kata/TicTacToe.java
+++ src/main/java/kata/TicTacToe.java
@@ -3,6 +3,11 @@ package kata;
 public class TicTacToe {
     public String play(int x, int y) {
 
+        if (x == 2){
+            return "_|_|X\n" +
+                    "_|_|_\n" +
+                    "_|_|_";
+        }
 
         if (x == 1){
             return "_|X|_\n" +
