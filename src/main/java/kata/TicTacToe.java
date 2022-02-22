3d2f99e028ce906106bb4c655a80b493157eae3f
oawood93 <oa.wood@outlook.com>
2022-02-22 20:54:45 +0000

    refactor

--- src/main/java/kata/TicTacToe.java
+++ src/main/java/kata/TicTacToe.java
@@ -4,19 +4,21 @@ public class TicTacToe {
 
     public String play(Point point) {
 
-        if (point.x() == 2){
-            return "_|_|X\n" +
-                    "_|_|_\n" +
-                    "_|_|_";
-        }
+        var rowOne = buildRow(point);
 
-        if (point.x() == 1){
-            return "_|X|_\n" +
-                    "_|_|_\n" +
-                    "_|_|_";
-        }
-        return "X|_|_\n" +
+        return rowOne + "\n" +
                 "_|_|_\n" +
                 "_|_|_";
+    }
+
+    private String buildRow(Point point) {
+        if (point.x() == 0){
+            return "X|_|_";
+        }
+        if (point.x() == 1){
+            return "_|X|_";
+        }
+
+        return "_|_|X";
     }
 }
